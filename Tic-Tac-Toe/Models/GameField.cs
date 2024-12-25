2b56a762ca1635b0be9a0881e126cc0bc1f15583
Ferhat Gümüş <ferhatgms@gmail.com>
2024-12-25 15:23:30 +0100

    Frontend und Model wurde erstellt

---
+++ Tic-Tac-Toe/Models/GameField.cs
@@ -0,0 +1,12 @@
+﻿namespace Tic_Tac_Toe.Models
+{
+    public class GameField
+    {
+        public List<Box> Fields { get; set; } = new List<Box>();
+    }
+
+    public class Box
+    {
+        public int BoxNumber { get; set; }
+    }
+}
---
+++ Tic-Tac-Toe/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace Tic_Tac_Toe.Models
+{
+    public class User
+    {
+        public string Name { get; set; }
+        public int PlayerNumber { get; set; }
+        public int Score { get; set; }
+    }
+}
